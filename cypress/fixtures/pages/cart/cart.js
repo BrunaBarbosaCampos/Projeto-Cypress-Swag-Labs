b5a31baad0afebca23cfce0b25fb379eb810e006
Bruna Natália Barbosa Campos <brunanataliacampos12602@gmail.com>
2025-02-06 10:49:56 -0300

    Dados para os cenários inseridos na pasta fixture

---
+++ cypress/fixtures/pages/cart/cart.js
@@ -0,0 +1,5 @@
+exports.information = {
+  name: "Fulano",
+  last_name: "de Tal",
+  postal_code: "12345678",
+};
---
+++ cypress/fixtures/pages/login/login.js
@@ -0,0 +1,10 @@
+exports.username = {
+  standard_user: "standard_user",
+  locked_out_user: "locked_out_user",
+  problem_user: "problem_user",
+  visual_user: "visual_user",
+};
+
+exports.password = {
+  default_password: "secret_sauce",
+};
